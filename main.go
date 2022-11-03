9015c5a45c98b0b7878175aba824af60bd1f3b19
Ruberald <sajidhossain909@gmail.com>
2022-11-03 21:39:06 +0530

    Removed unnecessary comments

--- main.go
+++ main.go
@@ -16,11 +16,9 @@ var reader = bufio.NewReader(os.Stdin)
 func main() {
 	fmt.Println("Enter the name of a film or show:")
 
-	// var name string 
 	name, _ := reader.ReadString('\n')
 	name = strings.TrimSpace(strings.Replace(name, " ", "+", -1))
 	fmt.Println(name)
-	// name = "Inception"
 
 	resp, _ := http.Get("http://www.omdbapi.com/?apikey=" + string(key) + "&t=" + name)
 
@@ -29,9 +27,6 @@ func main() {
 	  log.Fatalln(err)
 	}
 
-	//Convert the body to type string
 	sb := string(body)
 	log.Printf(sb)
-
-	// fmt.Println(details)
 }
